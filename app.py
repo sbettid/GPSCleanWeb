930720b9c7cfe9a7ae9cdeec4ae43721a9feaf2f
Sbetti Davide (Student Com16) <davide.sbetti@stud-inf.unibz.it>
2022-04-23 15:19:42 +0200

    Complete drag and drop functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 __name__ = 'GPSClean'
 
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def render_main():
     return render_template('main.html')
+
+@app.route("/correct_trace")
+def correct_trace(methods=['POST']):
+    return "Got " + request.form.get('file')
--- static/js/drag_drop.js
+++ static/js/drag_drop.js
@@ -1,17 +1,38 @@
+// Set form 
 var $form = $('.box');
 
-var droppedFiles = false;
-
+var droppedFiles = null;
+// prevent default behaviour on drag and drop events
 $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
-e.preventDefault();
-e.stopPropagation();
+    e.preventDefault();
+    e.stopPropagation();
 })
 .on('dragover dragenter', function() {
-$form.addClass('is-dragover');
+    $form.addClass('is-dragover'); //on drag over add custom CSS class
 })
 .on('dragleave dragend drop', function() {
-$form.removeClass('is-dragover');
+    $form.removeClass('is-dragover'); //and remove it when leaving the area
 })
-.on('drop', function(e) {
-droppedFiles = e.originalEvent.dataTransfer.files;
+.on('drop', function(e) { //on drop...
+    //Get files list
+    droppedFiles = e.originalEvent.dataTransfer.files;
+    
+    //Dropped file lis length
+    const droppedFilesLength = droppedFiles.length;
+    
+    if(droppedFilesLength > 1) {
+        alert("The tool can correct only one trace at the time, while " + droppedFilesLength + " files were uploaded, please retry just with one.");
+        return;
+    }
+
+    const trace = droppedFiles[0];
+    const extension = trace['name'].slice(trace['name'].lastIndexOf('.') + 1);
+    
+    if(extension.toLowerCase() != "gpx") {
+        alert("Only .gpx file can be corrected, while a ." + extension + " file was uploaded");
+        return;
+    }
+
+    //
+
 });
